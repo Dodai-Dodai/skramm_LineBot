0bcb636cf38fa50a9dc01e1d9e2d07ec892bfb0e
渡邉創大 <59632014nen@gmail.com>
2023-09-10 11:17:32 +0900

    コメントの追加

--- index.js
+++ index.js
@@ -9,15 +9,22 @@ const config = {
   const PORT = process.env.PORT || 3000;
 const app = express();
 
+
+// postされたときに実行される　handleEvent関数を実行
 app.post("/", middleware(config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent)).then((result) =>
     res.json(result)
   );
 });
 
+// サーバーを起動
 app.listen(PORT);
 
+// イベントを処理する関数
 function handleEvent(event) {
+    if (event.type !== "message" || event.message.type !== "text") {
+      return Promise.resolve(null);
+    }
     return client.replyMessage(event.replyToken, {
       type: "text",
       text: event.message.text,
