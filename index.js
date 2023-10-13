9b23870a9400a1084b23891b54472694597acf06
“{tsubocha}” <{ryosuketsubo.lit@gmail.com}>
2023-10-13 11:00:36 +0900

    正しく動くコード＋検証機能1

--- index.js
+++ index.js
@@ -1,44 +1,68 @@
 import express from "express";
 import { Client, middleware } from "@line/bot-sdk";
-const beaconMsg = "ビーコンを検知しました"
+
+const beaconMsg = "ビーコンを検知しました";
 const config = {
   channelSecret: process.env.CHANNEL_SECRET,
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 };
+
 const client = new Client(config);
 const PORT = parseInt(process.env.PORT) || 3000;
 const app = express();
+
 app.post("/", middleware(config), (req, res) => {
-  //リクエスト元のIPアドレスを取得
+  // リクエスト元のIPアドレスを取得
   console.log("=============ip=============");
   console.log(req.ip);
-  console.log(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
+  console.log(req.headers["x-forwarded-for"] || req.connection.remoteAddress);
   console.log("=============req.body=============");
   console.log(req.body);
   console.log("=============req.body.events=============");
   console.log(req.body.events);
   console.log("=============ここまで=============");
 
-  //署名を検証して正当なリクエストかどうか確認
-
-  /*if (!client.validateSignature(req.body, config.channelSecret)) {
-    return res.status(401).send("Invalid signature");
-  }
-  }*/
-
   // Promise.allで全てのイベント処理が終わるまで待機
   Promise.all(req.body.events.map(handleEvent)).then((result) =>
     res.json(result)
   );
 });
+
 app.listen(PORT);
-function handleEvent(event) {
+
+async function handleEvent(event) {
   if (event.type == "message" || event.message.type == "text") {
-    client.replyMessage(event.replyToken, {
-      type: "text",
-      text: event.message.text,
-    });
+    // ユーザのプロフィール情報を取得
+    const userId = event.source.userId;
+    const profile = await client.getProfile(userId);
+    const displayName = profile.displayName;
+
+    // プロフィール情報を使用して検証処理を行う
+    if (isLINEAccount(displayName, userId)) {
+      // LINEアカウントとして認識
+      client.replyMessage(event.replyToken, {
+        type: "text",
+        text: "LINEアカウントとして認識されました。",
+      });
+    } else {
+      // LINEアカウントとして認識されない
+      client.replyMessage(event.replyToken, {
+        type: "text",
+        text: "LINEアカウントとして認識されませんでした。",
+      });
+    }
   } else if (event.type == "beacon") {
     console.log("beaconを検知しました");
+  }
+}
+
+function isLINEAccount(displayName, userId) {
+  // プロフィール情報を元に検証
+  // ここで特定の条件に合致するかどうかを判定
+  // 例: 特定の表示名やユーザIDを持つユーザをLINEアカウントとして認識
+  if (displayName === "特定の表示名" || userId === "特定のユーザID") {
+    return true;
+  } else {
+    return false;
   }
 }
