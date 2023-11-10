97c486a78b9e65b1252bab7dcf939c4ce082e3ea
“{tsubocha}” <{ryosuketsubo.lit@gmail.com}>
2023-11-10 10:35:01 +0900

    リファクタリング第一段階

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ function handleEvent(event) {
       // 既に通知済みのユーザーにはメッセージを送信
       console.log("2度目です。");
       //2回目の受信であることを記録
-      incrementCount(userID)
+      incrementCount(countMap,userID);
       console.log(getCount(userID));
       client.replyMessage(event.replyToken, {
         type: "text",
