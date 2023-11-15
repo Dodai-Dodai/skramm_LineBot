2c0948d60d56745b72093721cde42cc3f2641ca7
Dodai-Dodai <59632014nen@gmail.com>
2023-11-15 22:49:08 +0900

    ログに重要度を指定できるように、condole.logではなく、logging.jsを作成してそれを用いることにする。\n現状は優先度をつけたのみで何もいじっていない

---
+++ logger/logging.js
@@ -0,0 +1,19 @@
+export function logger(message, severity) {
+    // serverityはログの重要度を表す文字列
+    // serverity: INFO, WARNING, ERROR, CRITICAL
+    // それ以外の文字列が入力された場合はINFOとして扱う
+    let severity = severity;
+    if (severity !== "INFO" && severity !== "WARNING" && severity !== "ERROR" && severity !== "CRITICAL") {
+        severity = "INFO";
+    }
+    else {
+        severity = severity;
+    }
+
+    const entry = {
+        severity: severity,
+        message: message,
+    };
+
+    console.log(JSON.stringify(entry));
+}
